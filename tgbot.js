61563c41f77a6cb6224ff9cbda61d3fb9ec81a98
Albert <salyakh0v@yandex.ru>
2020-03-21 13:59:19 +0300

    5th commit

--- tgbot.js
+++ tgbot.js
@@ -86,18 +86,88 @@ bot.onText (/Нечетная/, msg=> {
     })
 })
 
-bot.onText(/Понeдельник/, msg=> {
-    
-    const chatId = msg.chat.id
-    
-    bot.sendMessage(chatId, 'hello')
-})
-
 bot.onText(/Понедельник/, msg=> {
     
     const chatId = msg.chat.id
+    
+    bot.sendMessage(chatId, '1.ОиНХ, ДГНХ(Д-218)\n2.ОиНХ, ДГНХ(Д-218)\n3.ИГ(Д-503)')
+})
 
-    bot.sendMessage(chatId, 'Hi')
+bot.onText(/Вторник/, msg=> {
+    
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.-\n2.-\n3.Английский язык\n4.Английский язык\n5.ОХ, ДГОХ(Д-232)')
+})
+
+bot.onText(/Среда/, msg=> {
+
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.-\n2.Математика(Д-240)\n3.ОХ, ДГОХ(Д-322)\n4.ОХ, ДГОХ(Д-322)\n5.Английский язык\n6.Английский язык')
+})
+
+bot.onText(/Четверг/, msg=> {
+
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.Физика(Д-112)\n2.-\n3.-\n4.Физ-ра')
+})
+
+bot.onText(/Пятница/, msg=> {
+
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.-\n2.-\n3.Физика(Л-209)\n4.ОиНХ, ДГНХ(Д-232)\n5.ПМ(Д-203)')
+})
+
+bot.onText(/Суббота/, msg=> {
+
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.Математика(Д-416а)\n2.Математика(Д-416а)')
+})
+
+bot.onText(/Понeдельник/, msg=> {
+    
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.ОиНХ, ДГНХ(Д-218)\n2.ОиНХ, ДГНХ(Д-218)\n3.ИГ(Д-503)\n4.ВМ(Д-511)')
+})
+
+bot.onText(/Втoрник/, msg=> {
+
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.-\n2.-\n3.Английский язык\n4.Английский язык\n5.ОХ, ДГОХ(Д-232)')
+})
+
+bot.onText(/Срeда/, msg=> {
+
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.-\n2.Математика(Д-240)\n3.ВМ(Л-223)\n4.ОХ, ДГОХ(Д-322)\n5.Английский язык\n6.Английский язык')
+})
+
+bot.onText(/Чeтверг/, msg=> {
+
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.Физика(Д-112)\n2.-\n3.-\n4.Физ-ра')
+})
+
+bot.onText(/Пятницa/, msg=> {
+
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.-\n2.-\n3.-\n4.ОиНХ, ДГНХ(Д-232)\n5.ПМ(Д-203)\n6.Физика(Д-110)')
+})
+
+bot.onText(/Субботa/, msg=> {
+
+    const chatId = msg.chat.id
+
+    bot.sendMessage(chatId, '1.Математика(Д-416а)\n2.Математика(Д-416а)')
 })
 
 bot.onText(/Закрыть/, msg=> {
@@ -109,7 +179,7 @@ bot.onText(/Закрыть/, msg=> {
             remove_keyboard: true
         }
     })
-})
+}) 
     // if(msg.text === 'Расписание') {
     //     bot.sendMessage(chatId,'Выбери неделю', {
     //         reply_markup: {
@@ -138,3 +208,6 @@ bot.onText(/Закрыть/, msg=> {
 //     }
 
 // })
+
+ 
+ 
